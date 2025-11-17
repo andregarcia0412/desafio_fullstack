1b803f1137ab5180293243a69a7d7129e6bf24c9
andregarcia0412 <andregarcia0412@gmail.com>
2025-11-17 14:06:51 -0300

    refactoring

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,28 +1,27 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
-})
+  baseURL: "http://localhost:3000",
+});
 
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem("app_token")
-    if(token){
-        config.headers.Authorization = `Bearer ${JSON.parse(token).token}`
-    }
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("app_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${JSON.parse(token).token}`;
+  }
 
-    return config
-})
+  return config;
+});
 
 api.interceptors.response.use(
-    response => response,
-    error => {
-        if(error.response?.status == 401){
-            localStorage.removeItem("app_token")
-            window.location.href = "/auth"
-        }
-        return Promise.reject(error)
+  (response) => response,
+  (error) => {
+    if (error.response?.status == 401) {
+      localStorage.removeItem("app_token");
+      window.location.href = "/auth";
     }
-)
+    return Promise.reject(error);
+  }
+);
 
-
-export default api
+export default api;
