2c0e9b1ccbbce455b308b0a998c4e7c3c83b3571
andregarcia0412 <andregarcia0412@gmail.com>
2025-12-02 16:21:42 -0300

    final changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({origin:"desafio-fullstack-beryl.vercel.app"})
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
