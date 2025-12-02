de2d9b29e8d688294b2604f2e0f516eeb11d6102
andregarcia0412 <andregarcia0412@gmail.com>
2025-12-02 16:41:00 -0300

    adjust api address

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin:"desafio-fullstack-beryl.vercel.app"})
+  app.enableCors({origin:"https://desafio-fullstack-beryl.vercel.app"})
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://desafiofullstack-production.up.railway.app:3000",
+  baseURL: "https://desafiofullstack-production.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
